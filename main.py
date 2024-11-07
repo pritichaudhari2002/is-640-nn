bef06d768a4847060e53a4c64e874f353d718538
pritichaudhari2002 <pritichaudhari843@gmail.com>
2024-11-06 23:37:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ n = MLP(3, [4, 4, 1])
 for k in range(20):
     
     ypred = [n(x) for x in xs]
-    # Calculate loss as a Value object
+    
     loss = sum((yout - ygt) ** 2 for ygt, yout in zip(ys, ypred))
 
-    # Backward pass
+    
     for p in n.parameters():
         p.grad = 0.0
     loss.backward()  # All of the gradients are accumulated and start from zero

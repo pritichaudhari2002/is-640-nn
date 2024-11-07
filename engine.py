48d5a244d4da03c2d471b991b3684534f60dc6f7
pritichaudhari2002 <pritichaudhari843@gmail.com>
2024-11-06 23:34:40 -0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -74,10 +74,10 @@ class Value:
     def __sub__(self, other): 
         return self + (-other)
 
-    def __rsub__(self, other): # other - self
+    def __rsub__(self, other): 
         return other + (-self)
 
-    def __rmul__(self, other): # other * self
+    def __rmul__(self, other): 
         return self * other
     
     def __neg__(self): # -self
